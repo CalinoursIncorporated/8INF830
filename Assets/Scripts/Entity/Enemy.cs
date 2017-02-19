f365a46be08e868135a48dacff45c9e59059e5c5
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-02-18 20:01:49 -0500

    Enemi script
    Basic enemy script + IA script
    IA script calculate the desired force, enemy script call the ia script and use the calculated force
    Enemy can be destroyed ( As any Entity)
    
    Bonus : Projectil fired by primary weapon gain the velocity of the ship

--- Assets/Scripts/Entity/Enemy.cs
+++ Assets/Scripts/Entity/Enemy.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : Entity {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float MainThrust = 5.0f;
+    public float AuxThrust = 2.5f;
+    public float Torque = 2.5f;
+
+    BasicFlightControl iaEnemy;
+
+    Rigidbody rb;
+
+    protected override void Start()
+    {
+        base.Start();
+        iaEnemy = new BasicFlightControl(this);
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        Vector3 force = iaEnemy.CalculateForces();
+        Vector3 forwardForce = Vector3.Project(force, gameObject.transform.forward);
+        if(forwardForce.magnitude >= MainThrust)
+        {
+            forwardForce = forwardForce.normalized;
+            forwardForce *= MainThrust;
+        }
+        rb.AddForce(forwardForce);
+
+    }
+
 }
