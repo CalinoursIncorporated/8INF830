81ca63d2f423ccc10f718e098c9a5de38476cef6
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-03-11 22:14:32 -0400

    Wander Behavior

--- Assets/Scripts/Entity/Enemy.cs
+++ Assets/Scripts/Entity/Enemy.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class Enemy : Entity {
 
 
-
-    BasicFlightControl iaEnemy;
-
-    Rigidbody rb;
-
     protected override void Start()
     {
         base.Start();
-        iaEnemy = GetComponent<BasicFlightControl>();
-        rb = GetComponentInParent<Rigidbody>();
+
     }
 
     protected override void Update()
--- Assets/Scripts/Entity/EnemyIA/AbstratForce.cs
+++ Assets/Scripts/Entity/EnemyIA/AbstratForce.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class AbstratForce : MonoBehaviour {
 
-    public float Weight;
+    public float Weight = 15;
 
     public abstract Vector3 CalculateForce();
 }
