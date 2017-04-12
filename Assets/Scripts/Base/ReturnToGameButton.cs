5c1f501ced9d67f6868277c822e8a7d0ed7d3814
Moonvibes <antoine.ruffion@gmail.com>
2017-04-12 02:28:39 -0400

    Lots of colorful shit
    Added audio persistence through the base
    Added end screen for Hive Victory. Other screens will follow the same pattern.
    Added quest timer that runs only during gameplay.

--- Assets/Scripts/Base/ReturnToGameButton.cs
+++ Assets/Scripts/Base/ReturnToGameButton.cs
@@ -7,6 +7,14 @@ public class ReturnToGameButton : MonoBehaviour
 {
     public void Click(string sceneToLoad)
     {
+        if(GameObject.Find("BaseAudio"))
+        {
+            Destroy(GameObject.Find("BaseAudio"));
+        }
+        if (GameObject.Find("BaseAudio(Clone)"))
+        {
+            Destroy(GameObject.Find("BaseAudio(Clone)"));
+        }
         SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
     }
 }
